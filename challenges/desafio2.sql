8311f2b99570636cf7aac156f36a6c7a1142d553
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-04-04 19:27:39 -0300

    terminando requesito 2

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -1,7 +1,7 @@
 SELECT
   COUNT(m.music_id) AS "cancoes",
-  COUNT(a.artist_id) AS "artistas",
-  COUNT(al.album_id) AS "albuns"
+  COUNT(DISTINCT a.artist_id) AS "artistas",
+  COUNT(DISTINCT al.album_id) AS "albuns"
 FROM SpotifyClone.musics m
 JOIN SpotifyClone.albuns al ON m.album_id = al.album_id
 JOIN SpotifyClone.artist a ON al.artist_id = a.artist_id;
