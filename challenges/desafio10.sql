e7a25c03f359b73e04f28b2a478ef5c0b5428f9d
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-04-05 18:32:06 -0300

    wf

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -9,15 +9,15 @@ CREATE TABLE SpotifyClone.favorite_songs (
 INSERT INTO SpotifyClone.favorite_songs (user_id,music_id)
 VALUES
 (1, 3),
-  (1, 6),
-  (1, 10),
-  (2, 4),
-  (3, 1),
-  (3, 3),
-  (4, 7),
-  (4, 4),
-  (5, 10),
-  (5, 2),
-  (8, 4),
-  (9, 7),
-  (10, 3);
+(1, 6),
+(1, 10),
+(2, 4),
+(3, 1),
+(3, 3),
+(4, 7),
+(4, 4),
+(5, 10),
+(5, 2),
+(8, 4),
+(9, 7),
+(10, 3);
